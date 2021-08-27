494ec55fac51647240b33977fd8ccf6b0a479e8a
Binayak72 <binayakmahato01@gmail.com>
2021-08-27 18:28:50 +0545

    first commit

---
+++ resources/views/example.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>The sum of {{$firstval}} and {{$secondval}} is {{$firstval * $secondval}}</h1>
+
+</body>
+</html>
---
+++ resources/views/form.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <style type="text/css">
+
+        body{
+            background-image: url(http://www.joburgchiropractor.co.za/images/background.jpg);
+        }
+
+    </style>
+
+</head>
+<body>
+
+<div class="container" style="margin-top: 5%;">
+    <div class="row">
+        <div class="col-sm-4"> </div>
+        <div class="col-md-4">
+
+            <h1 class="text-center text-success"> Register page</h1>
+            <br/>
+
+            <div class="col-sm-12">
+
+                <br/>
+
+
+                <div class="tab-content">
+                    <div id="home" class="tab-pane fade in active">
+
+                        <form action="#">
+
+                            <div class="form-group">
+                                <label for="UserName">Username</label>
+                                <input type="text" class="form-control" id="email">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Email address:</label>
+                                <input type="email" class="form-control" id="email">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="pwd">Password:</label>
+                                <input type="password" class="form-control" id="pwd">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="pwd">Confirm Password:</label>
+                                <input type="password" class="form-control" id="pwd">
+                            </div>
+
+
+
+                            <button type="submit" class="btn btn-default btn-lg">Submit</button>
+                            <button type="submit" class=" pull-right btn-link"><a href="www.google.com">Forget password</a></button>
+
+                        </form>
+                        <br/>
+
+
+                    </div>
+
+
+    </div>
+</body>
+</html>
